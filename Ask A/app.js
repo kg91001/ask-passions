fdc071a2415e64fea94f0714b026ff25e6495ad8
kg91001 <87714042+kg91001@users.noreply.github.com>
2021-09-15 00:21:38 +0100

    Update app.js

--- Ask A/app.js
+++ Ask A/app.js
@@ -20,6 +20,7 @@
    function newsletter_subscribe() {
     var contactname = document.querySelector('.contact-name').value;
     var contactemail = document.querySelector('.email').value;
+    alert('Thank you for subscribing!');
 
        console.log(`Thank you ${contactname} for subscribing to the ASK Passions newsletter at ${contactemail}!`);
    }
