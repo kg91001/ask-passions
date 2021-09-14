b564f8f62833109f51f9c624894ce5f75d04339d
kg91001 <87714042+kg91001@users.noreply.github.com>
2021-09-14 16:22:27 +0100

    Update app.js

--- Ask A/app.js
+++ Ask A/app.js
@@ -18,21 +18,10 @@
  })
 
    function form_submission() {
-    alert('Thank you for submitting!');
     var contactname = document.querySelector('.contact-name').value;
     var contactemail = document.querySelector('.email').value;
     var contactask = document.querySelector('.ask').value;
     var contactmessage = document.querySelector('.message').value;
 
        console.log(`Thank you ${contactname} for contacting us at ${contactemail}! Your ASK for ${contactask} has been recorded: ${contactmessage}`);
-
-   }
-
-
-   function newsletter_subscribe() {
-    alert('Thank you for subscribing!');
-    var contactname = document.querySelector('.contact-name').value;
-    var contactemail = document.querySelector('.email').value;
-
-       console.log(`Thank you ${contactname} for subscribing to the ASK Passions newsletter at ${contactemail}!`);
    }
